687ba6f74ea9b8b6f4e5860fa5511b18a4902e84
csetiawan88 <csetiawan@yahoo.com>
2023-08-14 15:02:41 +1000

    Adding controller, DB, Models Public and Views Folders

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
-require("dotenv").config();
-
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
 let sequelize;
 
@@ -10,25 +9,13 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_PASSWORD,
     {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
     }
   );
 }
 
 module.exports = sequelize;
-
-// npm init
-// npm install express-session
-//npm install express
-//npm i sequelize
-//npm install connect-session-sequelize
-//npm i nodemon
-//npm i mysql2
-//npm i dotenv
-//npm install bcrypt
-//npm i handlebars
-//npm install express-handlebars
--- models/Comment.js
+++ models/Comment.js
@@ -1,46 +1,51 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
-Comment.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true,
+Comment.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull:false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        commentary: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'user',
+            key: 'id',
+          },
+        },
+        user_name: {
+            type:DataTypes.STRING,
+            allowNull: false
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            },
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
     },
-    comment_text: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [5], // Min 5 characters
-      },
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "user",
-        key: "id",
-      },
-    },
-    post_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "post",
-        key: "id",
-      },
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "comment",
-  }
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
 );
 
 module.exports = Comment;
