dabe187851961a6a709afeefa064c42513e8c1a1
csetiawan88 <csetiawan@yahoo.com>
2023-08-16 14:00:51 +1000

    Adding function to helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,12 @@
 module.exports = {
-    format_time: (date) => {
-        return date.toLocaleTimeString();
-      },
-    format_date: (date) => {
-      return `${new Date(date).getMonth()}/${new Date(date).getDate()}/  ${new Date(date).getFullYear()}`;
-    }
+  // the helper method 'format_time' will take in a timestamp and return a string with only the time
+  format_time: (date) => {
+    // We use the 'toLocaleTimeString()' method to format the time as H:MM:SS AM/PM
+    return date.toLocaleTimeString();
+  },
+  format_date: (date) => {
+    return `${new Date(date).getMonth()}/${new Date(
+      date
+    ).getDate()}/  ${new Date(date).getFullYear()}`;
+  },
 };
